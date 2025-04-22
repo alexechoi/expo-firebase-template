b6c5523d29733a84476a56001a9422841c36d735
alexchvod <uceiac8@ucl.ac.uk>
2025-04-22 22:58:46 +0100

    auth improvement

--- expo-app/app/_layout.tsx
+++ expo-app/app/_layout.tsx
@@ -1,14 +1,35 @@
 import { Stack } from "expo-router";
-import { AuthProvider } from "../contexts/AuthContext";
+import { AuthProvider, useAuth } from "../contexts/AuthContext";
+import { useEffect } from "react";
+import { SafeAreaView } from 'react-native';
+import { useRouter } from "expo-router";
 
 export default function RootLayout() {
   return (
     <AuthProvider>
+      <Main />
+    </AuthProvider>
+  );
+}
+
+function Main() {
+  const { user, loading } = useAuth();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!loading && user) {
+      // Redirect to Profile if user is logged in
+      router.replace('/profile');
+    }
+  }, [user, loading]);
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
       <Stack>
         <Stack.Screen name="index" options={{ headerShown: false }} />
         <Stack.Screen name="signup" options={{ title: "Sign Up", headerShown: false }} />
         <Stack.Screen name="profile" options={{ title: "Profile", headerShown: false }} />
       </Stack>
-    </AuthProvider>
+    </SafeAreaView>
   );
 }
